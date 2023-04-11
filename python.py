20de83293bb3c697beba32c16fc81b21f2578829
Ryan <Ryanhoss2013@gmail.com>
2023-04-11 19:34:19 +1200

    python.py has been created which add number to an input

---
+++ python.py
@@ -0,0 +1,17 @@
+# give me a number and I will add 5 to your number
+number=input("give me your number and i will add 25:")       
+integer_number=int(number)
+# print(number)  
+# print('This is your number with an added 5 numbers')
+NewNumber=integer_number+25
+print(NewNumber)
+# print(8+7)
+# print('8'+'7')
+# print('ryan'+"18")
+# print(type (number))
+# i='18'
+# print(type(i))
+# print(i)
+# integer_i=int(i)
+# print(type(integer_i))
+# print(integer_i)
