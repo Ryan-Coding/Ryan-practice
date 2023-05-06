ca114a851eba713dbdac624cebb1fcf0ae212395
Ryan <Ryanhoss2013@gmail.com>
2023-05-06 19:14:26 +1200

    today I created my own calculatar

--- python.py
+++ python.py
@@ -1,12 +1,29 @@
 # give me a number and I will add 5 to your number
 # lalalalalalalalalalalalalalalalalallaalalalallalaalalalllalaallalalala
-number=input("give me your number")       
-integer_number=int(number)
-number2=input('how much do you want to add?')
-interger_number2=int(number2)
-added_number=interger_number2+integer_number
-print('here is your number')
-print(added_number)
+number1=int(input("give me your first number:"))   
+number2=int(input("give me your second number:"))
+operation=input("do you want to add or subtract, divide or multiply:")
+if operation=="add":
+    result=number1+number2
+
+elif operation=="subtract":
+    result=number1-number2
+
+elif operation=="multiply":
+    result=number1*number2
+
+elif operation=="divide":
+    result=number1/number2
+else:
+    result="sorry,you can not do this"
+print(result)
+# print(number1,number2,operation)
+# integer_number=int(number)
+# number2=input('how much do you want to add?')
+# interger_number2=int(number2)
+# added_number=interger_number2+integer_number
+# print('here is your number')
+# print(added_number)
 # print(number)
 # print('This is your number with an added 5 numbers')
 # NewNumber=integer_number+25
