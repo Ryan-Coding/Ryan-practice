249f3d1130a059b1bf7cf94c4cdb8a71886ded9a
Ryan <Ryanhoss2013@gmail.com>
2023-05-22 19:47:00 +1200

    pushing to git with family.py

---
+++ family.py
@@ -0,0 +1,15 @@
+# familylist=["Shirin","Mabood","Milan","Ryan","Ali","Sarah"]
+# familyage=[42,43,6,10,38,40]
+# print(familylist)
+# print(familyage)
+# print(familylist[0])
+# print(familylist)
+# print(len(familylist))
+# print(familylist[1:4])
+# print(familylist[2:])
+classlist=['Alexander','Arthur','Justin','Luka','Cailey','Samantha','Annabele','paige','Rebecca','Marlo','Zach','Quintin','Illisa','Sophie','Blake','Allina','Junyi','Christine','Willow','Suri','Jason','Aiden','Nathan','Kohana','Quarra','Theo','Will']
+print(input('Tell me any student in my class'))
+if input==classlist
+     result=print('I know that person, they are in my class')
+else:
+     print('I dont know that person, they are not in my class')
